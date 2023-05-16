a18a620183f454a700c343015ce1fa8b320b0b23
Nastya N <asrozova@gmail.com>
2023-05-16 18:49:04 +0200

    feature:like_button/begin

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const editButton = document.querySelector('.profile-header__edit-button');
 const popupEdit = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__button-close');
+const formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_info');
+const saveButton = document.querySelector('.popup__button-save');
 
 function popupOpen() {
     console.log('I ve been clicked');
@@ -11,16 +15,6 @@ function popupClose() {
     console.log('I ve been closed');
     popupEdit.classList.remove('popup_opened');
 };
-
-editButton.addEventListener('click', popupOpen);
-closePopupButton.addEventListener('click', popupClose);
-
-const formElement = document.querySelector('.popup__container');
-
-const nameInput = document.querySelector('.popup__input_name');
-const jobInput = document.querySelector('.popup__input_info');
-
-const saveButton = document.querySelector('.popup__button-save');
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
@@ -34,5 +28,21 @@ function handleFormSubmit(evt) {
     console.log('I ve been saved');
 }
 
+editButton.addEventListener('click', popupOpen);
+closePopupButton.addEventListener('click', popupClose);
+
 formElement.addEventListener('submit', handleFormSubmit); 
 saveButton.addEventListener('click', popupClose);
+
+const likeButtons = document.querySelectorAll('.gellary__like-button');
+const likeArray = Array.from(likeButtons);
+
+likeArray.forEach(function (item) {
+    console.log('I ve been liked');
+    item.classList.toggle('gellary__like-button_active');
+});
+
+item.addEventListener('click', likeActive);
+
+
+
