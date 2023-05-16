50c2a43dd04cec8f3668e35aa4034778f92a17ea
Nastya N <asrozova@gmail.com>
2023-05-16 17:09:14 +0200

    feature:add name/rough

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ function popupClose() {
     popupEdit.classList.remove('popup_opened');
 };
 
-
 editButton.addEventListener('click', popupOpen);
 closePopupButton.addEventListener('click', popupClose);
+
+const formElement = document.querySelector('.popup__container');
+
+const nameInput = document.querySelector('.popup__input_name');
+const jobInput = document.querySelector('.popup__input_info');
+
+const saveButton = document.querySelector('.popup__button-save');
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+  
+    const userName = document.querySelector('.profile-header__user-name');
+    const userInfo = document.querySelector('.profile-header__user-description');
+    
+    userName.textContent = nameInput.value;
+    userInfo.textContent = jobInput.value;
+
+    console.log('I ve been saved');
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+saveButton.addEventListener('click', popupClose);
