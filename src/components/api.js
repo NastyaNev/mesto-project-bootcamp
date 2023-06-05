56cd940d2eb31020585c3ea96e53e3e00501a12b
Nastya N <asrozova@gmail.com>
2023-06-05 20:34:51 +0200

    feat:get_cards_from_api

--- src/components/api.js
+++ src/components/api.js
@@ -9,7 +9,22 @@ export function getUserInfo() {
             if (res.ok) {
                 return res.json();
             }
+            return Promise.reject(`Ошибка: ${res.status}`);
+        })
+}
 
+
+export function getCards() {
+    return fetch('https://nomoreparties.co/v1/wbf-cohort-9/cards', {
+        headers: {
+            authorization: '074a28df-5a24-4ba3-ac4e-c10822c2f568',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
             return Promise.reject(`Ошибка: ${res.status}`);
         })
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,69 +1,76 @@
 export const galleryContainer = document.querySelector('.gallery');
 const galleryTemplate = document.getElementById('gallery__template').content.querySelector('.gallery__item');
 
-const initialCards = [
-    {
-      name: 'Пушкин',
-      link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Pushkin.jpg'
-    },
-    {
-      name: 'Рускеала',
-      link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Ruskeala.jpg'
-    },
-    {
-      name: 'Санкт-Петербург',
-      link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Saint-P.jpg'
-    },
-    {
-      name: 'Тургояк',
-      link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Tyrgoyak.jpg'
-    },
-    {
-      name: 'Ульяновск',
-      link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Ulyanovsk.jpg'
-    },
-    {
-      name: 'Псков',
-      link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Pskov.jpg'
-    }
-  ]; 
+// const initialCards = [
+//   {
+//     name: 'Пушкин',
+//     link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Pushkin.jpg'
+//   },
+//   {
+//     name: 'Рускеала',
+//     link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Ruskeala.jpg'
+//   },
+//   {
+//     name: 'Санкт-Петербург',
+//     link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Saint-P.jpg'
+//   },
+//   {
+//     name: 'Тургояк',
+//     link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Tyrgoyak.jpg'
+//   },
+//   {
+//     name: 'Ульяновск',
+//     link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Ulyanovsk.jpg'
+//   },
+//   {
+//     name: 'Псков',
+//     link: 'https://nastyanpictures1.s3.eu-north-1.amazonaws.com/Pskov.jpg'
+//   }
+// ];
 
 function deleteElement(element) {
-    element.remove();
+  element.remove();
 
-    console.log("I've been deleted");
+  console.log("I've been deleted");
 }
 
 function likeElement(likeButton) {
-    likeButton.classList.toggle('gellary__like-button_active');
+  likeButton.classList.toggle('gellary__like-button_active');
 
-    console.log("I've been liked");
+  console.log("I've been liked");
 }
 
 export function addGalleryElement(name, link) {
-    const galleryElement = galleryTemplate.cloneNode(true);
+  const galleryElement = galleryTemplate.cloneNode(true);
 
-    const galaryLink = galleryElement.querySelector('.gallery__photo');
-    const galaryName = galleryElement.querySelector('.gallery__caption');
+  const galaryLink = galleryElement.querySelector('.gallery__photo');
+  const galaryName = galleryElement.querySelector('.gallery__caption');
 
-    galaryLink.src = link;
-    galaryName.textContent = name;
-    galaryLink.alt = name;
+  galaryLink.src = link;
+  galaryName.textContent = name;
+  galaryLink.alt = name;
 
-    const deleteButton = galleryElement.querySelector('.gallery__delete-button');
-    const likeButton = galleryElement.querySelector('.gellary__like-button');
-    const openWideButton = galleryElement.querySelector('.gallery__photo-button');
+  const deleteButton = galleryElement.querySelector('.gallery__delete-button');
+  const likeButton = galleryElement.querySelector('.gellary__like-button');
+  const openWideButton = galleryElement.querySelector('.gallery__photo-button');
 
-    deleteButton.addEventListener('click', () => deleteElement(galleryElement));
-    likeButton.addEventListener('click', () => likeElement(likeButton));
-    openWideButton.addEventListener('click', () => openPhotoPopup(name, link));
+  deleteButton.addEventListener('click', () => deleteElement(galleryElement));
+  likeButton.addEventListener('click', () => likeElement(likeButton));
+  openWideButton.addEventListener('click', () => openPhotoPopup(name, link));
 
-    return galleryElement;
+  return galleryElement;
 }
 
-initialCards.forEach(function (element) {
+getCards()
+  .then(res => {
+    res.forEach(function (element) {
     const newGalleryElement = addGalleryElement(element.name, element.link);
     galleryContainer.append(newGalleryElement);
-});
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 import { openPhotoPopup } from '../index';
+import { getCards } from '../components/api';
