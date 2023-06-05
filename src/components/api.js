d5ea679a2f78561364a621a596ce42dab933f8e3
Nastya N <asrozova@gmail.com>
2023-06-05 21:42:37 +0200

    refactor:config&checkResponse

--- src/components/api.js
+++ src/components/api.js
@@ -1,70 +1,53 @@
+const config = {
+    baseUrl: 'https://nomoreparties.co/v1/wbf-cohort-9',
+    headers: {
+        authorization: '074a28df-5a24-4ba3-ac4e-c10822c2f568',
+        'Content-Type': 'application/json'
+    }
+}
+
+function checkResponse(res) {
+    if (res.ok) {
+        return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+}
+
 export function getUserInfo() {
-    return fetch('https://nomoreparties.co/v1/wbf-cohort-9/users/me', {
-        headers: {
-            authorization: '074a28df-5a24-4ba3-ac4e-c10822c2f568',
-            'Content-Type': 'application/json'
-        }
+    return fetch(`${config.baseUrl}/users/me`, {
+        headers: config.headers
     })
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-            return Promise.reject(`Ошибка: ${res.status}`);
-        })
+        .then(checkResponse)
 }
 
 export function setUserInfo(name, about) {
-    return fetch('https://nomoreparties.co/v1/wbf-cohort-9/users/me', {
-        headers: {
-            authorization: '074a28df-5a24-4ba3-ac4e-c10822c2f568',
-            'Content-Type': 'application/json'
-        },
+    return fetch(`${config.baseUrl}/users/me`, {
+        headers: config.headers,
         method: 'PATCH',
         body: JSON.stringify({
             name: name,
             about: about
         })
     })
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-            return Promise.reject(`Ошибка: ${res.status}`);
-        })
+        .then(checkResponse)
 }
 
 
 export function getCards() {
-    return fetch('https://nomoreparties.co/v1/wbf-cohort-9/cards', {
-        headers: {
-            authorization: '074a28df-5a24-4ba3-ac4e-c10822c2f568',
-            'Content-Type': 'application/json'
-        }
+    return fetch(`${config.baseUrl}/cards`, {
+        headers: config.headers
     })
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-            return Promise.reject(`Ошибка: ${res.status}`);
-        })
+        .then(checkResponse)
 }
 
 export function setCards(name, link) {
-    return fetch('https://nomoreparties.co/v1/wbf-cohort-9/cards', {
-        headers: {
-            authorization: '074a28df-5a24-4ba3-ac4e-c10822c2f568',
-            'Content-Type': 'application/json'
-        },
+    return fetch(`${config.baseUrl}/cards`, {
+        headers: config.headers,
         method: 'POST',
         body: JSON.stringify({
             name: name,
             link: link
         })
     })
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-            return Promise.reject(`Ошибка: ${res.status}`);
-        })
+        .then(checkResponse)
 }
